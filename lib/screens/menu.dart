3a2194fd89451c1193d37629c8d98f06a77f562b
gilbertkristiaan <gilbertkristian79@gmail.com>
2024-12-04 15:46:32 +0700

    update menu.dart

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -19,9 +19,59 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       drawer: const LeftDrawer(),
-      body: const Center(
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Welcome to MAKANSAR!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: [
+                _buildButton('Ayam'),
+                _buildButton('Daging'),
+                _buildButton('Chinese Food'),
+                _buildButton('Arabic Food'),
+                _buildButton('Dessert'),
+                _buildButton('Makanan Berkuah'),
+                _buildButton('Nasi'),
+                _buildButton('Seafood'),
+                _buildButton('Martabak'),
+                _buildButton('Beverages'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String label) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+        ),
+        onPressed: () {
+          // Tambahkan logika untuk menangani klik tombol di sini
+        },
         child: Text(
-          'Welcome to MAKANSAR!',
+          label,
+          style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
